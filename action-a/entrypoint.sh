527b2dfe56176757cb3a21612ebeedd960e7668d
alicezhiyingwang <76703910+alicezhiyingwang@users.noreply.github.com>
2021-01-01 12:10:21 +0000

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 #The entrypoint.sh script will be run in Docker, and it will define what the action is really going to be doing.
 
-sh -c "echo Hello there my name is $INPUT_MY_NAME"
+sh -c "echo Hello there my name is $INPUT_MY_NAME  $INPUT_MY_LAST_NAME"
